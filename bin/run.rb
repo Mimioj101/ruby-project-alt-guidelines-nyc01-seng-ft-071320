62478b2bf2b8f16785eeeff708be88233ae26999
Mimioj101 <mimioj@gmail.com>
2020-07-29 16:07:53 -0400

    sign up, login, started write_review method

--- bin/run.rb
+++ bin/run.rb
@@ -1,8 +1,8 @@
 require_relative '../config/environment'
 
 cli = CommandLineInterface.new
+
 cli.greet
 
 User.login
-cli.menu
-U
+
--- lib/app/models/cli.rb
+++ lib/app/models/cli.rb
@@ -8,20 +8,40 @@ class CommandLineInterface
         puts "Welcome to Filmbuff, your review is important to us"
     end 
 
-    def menu
+    def self.menu
         puts "What would you like to do?"
         puts "Put 1 to write, edit or delete a review!"
         puts "Put 2 to look up a movie!"
         puts "Put 3 to look at your reviews!"
+        user_input = gets.chomp
+        case user_input
+        when "1"
+            self.review_menu
+        when "2"
+            #search_movie
+        when "3"
+            #view_own_reviews
+        else
+            menu
+        end
+    end
 
-    #     user_input= gets.chomp
-    #     if user_input == 2
-    #         movies.title
-    #     if user_input== 3
-    #        puts  user.movie_rating && user.review_comment
-    #     end 
-
-    # end 
+    def self.review_menu
+        puts "Put 1 to write a review!"
+        puts "Put 2 to edit a review!"
+        puts "Put 3 to delete a review."
+        user_input = gets.chomp
+        case user_input
+        when "1"
+            User.write_review
+        when "2"
+            User.edit_review
+        when "3"
+            User.delete_review
+        else
+           self.review_menu
+        end
+    end
     
 
 
