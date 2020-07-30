a890215be1c5505eb74e4538a8eb6bb5d53b5f54
Mimioj101 <mimioj@gmail.com>
2020-07-30 11:54:46 -0400

    debugging see_my_reviews

--- lib/app/models/cli.rb
+++ lib/app/models/cli.rb
@@ -7,6 +7,7 @@ class CommandLineInterface
         greet
         puts "Type in your username to login or signup:"
         username = gets.chomp
+        puts "\n"
         puts "Type in your password to login or signup:"
         password = gets.chomp
         @user = User.find_or_create_by(username: username, password: password)
@@ -19,9 +20,14 @@ class CommandLineInterface
 
 
     def menu
+        puts "\n"
         puts "What would you like to do?"
+            puts "\n"
+            puts "\n"
         puts "Put 1 to write, edit or delete a review!"
+        puts "\n"
         puts "Put 2 to look up a movie!"
+        puts "\n"
         puts "Put 3 to look at your reviews!"
         user_input = gets.chomp
         case user_input
@@ -31,14 +37,15 @@ class CommandLineInterface
             search_movie
         when "3"
             see_my_reviews
-        else
-            menu
         end
+        menu
     end
 
     def review_menu
         puts "Put 1 to write a review!"
+        puts "\n"
         puts "Put 2 to edit a review!"
+        puts "\n"
         puts "Put 3 to delete a review."
         user_input = gets.chomp
         case user_input
@@ -49,7 +56,7 @@ class CommandLineInterface
         when "3"
             delete_review
         else
-           review_menu
+            menu
         end
     end
 
@@ -57,23 +64,31 @@ class CommandLineInterface
     def write_review
         puts "What movie would you like to write a review for?"
             title = gets.chomp
-            movie_id = Movie.where(title: title).ids
-                if Movie.exists?(title: title)
-                    puts "Awesome! Out of 5 stars, how many stars would you rate #{title}?"
-                    movie_rating = gets.chomp
-                    puts "What would you like to write about #{title}?"
-                    review_comment = gets.chomp
-                    Review.create(movie_id: movie_id, user_id: @user.id, movie_rating: movie_rating, review_comment: review_comment)
+            puts "\n"
+            movie = Movie.find_by(title: title)
+            # movie_id = Movie.where(title: title).ids
+            if Movie.exists?(title: title)
+                puts "Awesome! Out of 5 stars, how many stars would you rate #{title}?"
+                movie_rating = gets.chomp
+                puts "\n"
+                puts "What would you like to write about #{title}?"
+                review_comment = gets.chomp
+                puts "\n"
+                Review.create(movie_id: movie.id, user_id: @user.id, movie_rating: movie_rating, review_comment: review_comment)
                 else
                     puts "What genre is #{title}?"
                     genre = gets.chomp
+                    puts "\n"
                     puts "How many minutes is #{title}?"
                     length = gets.chomp
+                    puts "\n"
                     new_mov = Movie.create(title: title, genre: genre, length: length)
                     puts "Awesome! Out of 5 stars, how many stars would you rate #{title}?"
                     movie_rating = gets.chomp
+                    puts "\n"
                     puts "What would you like to write about #{title}?"
                     review_comment = gets.chomp
+                    puts "\n"
                     Review.create(movie_id: new_mov.id, user_id: @user.id, movie_rating: movie_rating, review_comment: review_comment)
                 end
         puts "Your review is being published! Thanks so much for contributing to MovieBuff!"
@@ -84,22 +99,28 @@ class CommandLineInterface
     def edit_review
         puts "What is the title of the movie that you reviewed and want to edit?"
         title = gets.chomp
+        puts "\n"
         movie_id = Movie.where(title: title).ids
         review = Review.find_by(movie_id: movie_id, user_id: @user.id)
         puts "what would you like to update?"
+        puts "\n"
         puts "put 1 to update the movie rating"
+        puts "\n"
         puts "put 2 to update the review comment"
         user_input = gets.chomp
         case user_input
         when "1"
             puts "What would you like your updated rating to be?"
             input = gets.chomp
+            puts "\n"
             review.update(movie_rating: input)
             puts "Your review is now updated!"
+            puts "\n"
             menu
         when "2"
             puts "What would you like your updated review comment to be?"
             input = gets.chomp
+            puts "\n"
             review.update(review_comment: input)
             puts "Your review is now updated!"
             menu
@@ -107,17 +128,20 @@ class CommandLineInterface
     end
 
     def delete_review
+        puts "\n"
         puts "What is the title of the movie that you reviewed and want to delete?"
         title = gets.chomp
+        puts "\n"
         movie_id = Movie.where(title: title).ids
         review = Review.find_by(movie_id: movie_id, user_id: @user.id)
         puts "Are you sure you would like to delete your #{review.movie_rating} star review saying '#{review.review_comment}'? Y/N?"
         user_input = gets.chomp
+        puts "\n"
         case user_input
         when "Y"
             Review.delete(review.id)
-            #NOT WORKING?
             puts "Your review has been deleted. What else can we help you with?"
+            puts "\n"
             menu
         when "N"
             puts "Changed your mind? Anything else we can help you with?"
@@ -128,24 +152,31 @@ class CommandLineInterface
 
     def search_movie
         puts "Enter the title of the movie you would like to view:"
-        user_input = gets.chomp
-        movie_id = Movie.where(title: user_input).ids
-        Review.all.where(movie_id: movie_id)
+        title = gets.chomp
+        puts "\n"
+        # movie_id = Movie.where(title: title).ids
+        movie = Movie.find_by(title: title)
+        # review = Review.where(movie_id: movie.id)
+        # movie.each {|ele| puts  "Movie: #{movie.title}, Genre: #{movie.genre}, Length: #{movie.length}, Rating: #{review.movie_rating}, Review: #{review.review_comment}"}
+        puts  "Movie: #{movie.title}, Genre: #{movie.genre}, Length: #{movie.length}"
+        puts "\n"
+        menu
     end
+    #Erroring out, breaking app, not puts-ing string
 
 
     def see_my_reviews
+        puts "\n"
         Review.all.each do |review|
             if review.user_id == @user.id
                 mov = review.movie_id
                 movie = Movie.find(mov)
-                puts "Movie: #{movie.title} Rating: #{review.movie_rating} Review: #{review.review_comment}"
-                # binding.pry
+                puts "Movie: #{movie.title}, Rating: #{review.movie_rating}, Review: #{review.review_comment}"
+                puts "Bringing you back to the main menu now!"
             end
         end
-        
+        # menu
     end
-    
 
 
         
